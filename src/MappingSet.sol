72103a4c6f7a06d44ef7db9a04cdc6cf22eeaced
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-02-09 22:53:11 +0100

    20. MappingSetTest done

--- src/MappingSet.sol
+++ src/MappingSet.sol
@@ -2,20 +2,20 @@
 pragma solidity 0.8.28;
 
 library StorageSetLib {
-
     // implement the function increment(...)
     // it should increment the value at key 3
     // implement increment3(...)
+    function increment3(mapping(uint256 => uint256) storage m) internal {
+        m[3] = m[3] + 1;
+    }
 }
 
-
 contract MappingSet {
-
     mapping(uint256 => uint256) public m;
 
     using StorageSetLib for mapping(uint256 => uint256);
 
     function main() public {
-        //m.increment3();
+        m.increment3();
     }
 }
