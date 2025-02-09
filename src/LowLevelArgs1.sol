c2ffb97f52587709155929d248c537245fe6a534
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-02-09 22:18:13 +0100

    12. LowLevelArgs1 done

--- src/LowLevelArgs1.sol
+++ src/LowLevelArgs1.sol
@@ -6,6 +6,18 @@ contract LowLevelArgs1 {
         // call rare(x) using a low-level call
         // if the low level call reverts, revert also
 
+        assembly {
+            mstore(0x00, 0xa5487e51)
+            mstore(0x20, x)
+            let ok := call(gas(), a, 0, 0x1c, 0x24, 0, 0)
+            if iszero(ok) {
+                returndatacopy(0x00, 0x00, returndatasize())
+                revert(0x00, returndatasize())
+            }
+        }
+
         // bonus challenge: use an interface and a high level call to accomplish the same task
+        //(bool success,) = a.call(abi.encodeWithSignature("rare(uint256)", x));
+        //if (!success) revert();
     }
 }
