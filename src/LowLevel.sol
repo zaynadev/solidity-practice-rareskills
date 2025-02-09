e3611f2a70e2c8b0eea729b54a3a8199a1b91875
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-02-09 22:06:38 +0100

    11. LowLevel done

--- src/LowLevel.sol
+++ src/LowLevel.sol
@@ -8,7 +8,17 @@ contract LowLevel {
         // return true if the call succeeded
         // return false if the call failed
 
-        // bonus challenge: use an interface and a high level call to accomplish the same task
+        assembly {
+            mstore(0x00, 0xc2985578)
+            let ok := call(gas(), a, 0, 0x1c, 0x04, 0, 0)
+            mstore(0x00, ok)
+            return(0x00, 0x20)
+        }
 
+        // bonus challenge: use an interface and a high level call to accomplish the same task
+        /*
+        (bool success,) = a.call(abi.encodeWithSignature("foo()"));
+        return success;
+        */
     }
 }
