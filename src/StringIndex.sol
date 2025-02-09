0aafe8670fcd6b115325fec0d4713a2769de22b0
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-02-09 20:54:09 +0100

    5. StringIndex done

--- src/StringIndex.sol
+++ src/StringIndex.sol
@@ -5,5 +5,9 @@ contract StringIndex {
     function main(string memory str, uint256 index) public returns (string memory) {
         // return the character at index in str
         // assume str only consists of ascii characters
-    } 
+        //bytes memory str = bytes(str);
+        bytes memory char = new bytes(1);
+        char[0] = bytes(str)[index];
+        return string(char);
+    }
 }
