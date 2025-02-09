1942b695377702849762bac1bedc50447bb727f8
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-02-09 20:10:15 +0100

    3. MemoryArray done

--- src/MemoryArray.sol
+++ src/MemoryArray.sol
@@ -5,6 +5,10 @@ contract MemoryArray {
     function main(uint8 a) public returns (uint256[] memory) {
         // return an array that goes from 0 to a
         // for example, if a is 3, return [0, 1, 2]
-
+        uint256[] memory arr = new uint256[](a);
+        for (uint8 i; i < a; i++) {
+            arr[i] = i;
+        }
+        return arr;
     }
 }
