6929b531f7fa8620132160a8c965334321ea9f59
Zainab Oufqir <zayna.oufkir@gmail.com>
2025-02-09 21:34:30 +0100

    8. TryCatch done

--- src/TryCatchSimple.sol
+++ src/TryCatchSimple.sol
@@ -8,5 +8,11 @@ contract TryCatchSimple {
     function main(IERC20 token) public view returns (string memory) {
         // use try catch to return the name of the token using .name()
         // if the call fails, return an empty string
+        try token.name() returns (string memory name) {
+            return name;
+        } catch {
+            bytes memory empty = new bytes(0);
+            return string(empty);
+        }
     }
 }
