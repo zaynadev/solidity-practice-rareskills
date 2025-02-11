7ff0bd9ae63555fdf77043438d15d9ef55c9bc4c
Jeffrey Scholz <scholz.jeffrey@gmail.com>
2025-02-11 06:14:05 -0800

    Update MemoryArray.sol

--- src/MemoryArray.sol
+++ src/MemoryArray.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.28;
 
 contract MemoryArray {
     function main(uint8 a) public returns (uint256[] memory) {
-        // return an array that goes from 0 to 0
+        // return an array that goes from 0 to a
         // for example, if a is 3, return [0, 1, 2]
 
     }
